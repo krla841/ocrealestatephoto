77daac07766d94f18a0db8f758e6cfc584e70307
Karla Sosa <karla.sosa@avl.com>
2024-11-26 21:18:19 -0800

    nav bar mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
 import { Router } from '@angular/router';
-
+declare var M: any;
 
 @Component({
   selector: 'app-root',
@@ -12,8 +12,22 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   title = 'realestatephoto-angular';
+  sidenavInstance: any;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) { }
+
+  ngAfterViewInit() {
+    const elems = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(elems);
+    this.sidenavInstance = M.Sidenav.init(elems)[0];
+
+    // Listen for route changes and close sidenav
+    this.router.events.subscribe(() => {
+      if (this.sidenavInstance && this.sidenavInstance.isOpen) {
+        this.sidenavInstance.close();
+      }
+    });
+  }
 
   navigateToPortfolio(): void {
     this.router.navigate(['/home']);
